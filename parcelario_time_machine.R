0ed67be9d86d1190fdce18712c39f623f67145c4
Santi Soubie <ssoubie@mail.utdt.edu>
2021-11-25 02:08:53 -0300

    Parcelas voladoras

---
+++ parcelario_time_machine.R
@@ -0,0 +1,51 @@
+library(tidyverse)
+library(sf)
+sf::sf_use_s2(FALSE)
+
+parcelario <- st_read("data/raw/GCABA/Parcelario/210903_Parcelario_SSREGIC_DGAUR.shp")
+
+radios_CABA <- st_read("data/raw/INDEC/cabaxrdatos.shp", stringsAsFactors = FALSE)
+
+cluster16 <- st_read("data/processed/accesibilidad/radios_cluster_16.shp") %>% 
+    st_transform(st_crs(4326))
+
+parcelario <-  parcelario %>% 
+    st_transform(st_crs(4326))
+
+radios_CABA <- radios_CABA %>% 
+    st_transform(st_crs(parcelario))
+
+ggplot()+
+    geom_sf(data = cluster16, color="black")+
+    geom_sf(data = parcelario_16, fill=NA, color="red")+
+    geom_sf(data = estacionamientos, size=0.5)
+    
+    
+st_crs(parcelario)
+
+geo_poligonos <- st_geometry(parcelario)
+
+nueva_geo <- geo_poligonos + c(-0.0006, 0.00045)
+
+parcelario <- parcelario %>% 
+    st_set_geometry(nueva_geo)
+
+parcelario <- parcelario %>% 
+    st_set_crs(4326)
+
+parcelario_16 <- parcelario %>% 
+    st_intersection(cluster16) %>% 
+    unique()
+
+
+estacionamientos <- st_read("data/processed/osm/estacionamientos_relevados.shp") %>% #infraestuctura vacante, oferta potencial
+    st_transform(crs=4326)
+
+library(leaflet)
+
+leaflet(parcelario_16) %>% 
+    addTiles() %>% 
+    addPolygons()
+
+write_sf(parcelario_16, "parcelario_cluster_16.shp")
+    
