949c2b7ab80f7adbbacf3379b24232535598e81f
Santi Soubie <ssoubie@mail.utdt.edu>
2021-11-30 16:23:03 -0300

    Retoques estacionamientos

--- parcelario_time_machine.R
+++ parcelario_time_machine.R
@@ -9,6 +9,9 @@ radios_CABA <- st_read("data/raw/INDEC/cabaxrdatos.shp", stringsAsFactors = FALS
 cluster16 <- st_read("data/processed/accesibilidad/radios_cluster_16.shp") %>% 
     st_transform(st_crs(4326))
 
+cluster12 <- st_read("data/processed/accesibilidad/radios_cluster_12.shp") %>% 
+    st_transform(crs = 4326)
+
 parcelario <-  parcelario %>% 
     st_transform(st_crs(4326))
 
@@ -37,7 +40,6 @@ parcelario_16 <- parcelario %>%
     st_intersection(cluster16) %>% 
     unique()
 
-
 estacionamientos <- st_read("data/processed/osm/estacionamientos_relevados.shp") %>% #infraestuctura vacante, oferta potencial
     st_transform(crs=4326)
 
@@ -49,3 +51,29 @@ leaflet(parcelario_16) %>%
 
 write_sf(parcelario_16, "parcelario_cluster_16.shp")
     
+# Cluster 12
+
+geo_poligonos <- st_geometry(parcelario)
+
+nueva_geo <- geo_poligonos + c(-0.0006, 0.00043)
+
+parcelario <- parcelario %>% 
+    st_set_geometry(nueva_geo)
+
+parcelario <- parcelario %>% 
+    st_set_crs(4326)
+
+parcelario_12 <- parcelario %>% 
+    st_intersection(cluster12) %>% 
+    unique()
+
+ggplot()+
+    geom_sf(data = cluster12, color="black")+
+    geom_sf(data = parcelario_12, fill=NA, color="red")
+
+leaflet(parcelario_12) %>% 
+    addTiles() %>% 
+    addPolygons()
+
+write_sf(parcelario_12, "parcelario_cluster_12.shp")
+
