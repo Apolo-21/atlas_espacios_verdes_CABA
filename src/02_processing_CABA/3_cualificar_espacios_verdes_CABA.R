0a0a98e945ca9b7c4a2caa025957d0b5dd85e969
SSoubie <ssoubie@mail.utdt.edu>
2022-02-28 17:32:36 -0300

    OSM vs BA Data

--- src/02_processing_CABA/3_cualificar_espacios_verdes_CABA.R
+++ src/02_processing_CABA/3_cualificar_espacios_verdes_CABA.R
@@ -3,35 +3,36 @@ library(sf)
 library(leaflet)
 
 
-##############################################################################################
-# Ponderación de la superficie de espacios verdes de acuerdo a su condición de accesibilidad #
-##############################################################################################
+################################################################################################
+# Ponderación de la superficie de espacios verdes de acuerdo con su condición de accesibilidad #
+################################################################################################
 
-# Los espacios enrejados de la ciudad abren sus puertas entre 08:00 hs y 20:00 hs.
-# Como consecuencia, solo es posible acceder a los mismos entre dichas horas.
-# En base a esta situación, optamos por subponderar la superficie de los mismos 
-# con el fin de que nuestro indice de accesebilidad a EV de cuenta de esta restricción
-# temporal a la que se ven sujetos los vecinos de la CABA.
+# Los espacios verdes (EV) enrejados de la ciudad de Buenos Aires (CABA) abren sus
+# puertas, generalmente, entre 08:00 hs y 20:00 hs. Como consecuencia, solo es posible 
+# acceder a los mismos entre dichas horas. En base a esta situación, optamos por
+# subponderar la superficie de los mismos con el fin de que nuestro indice de accesebilidad 
+# a EV de cuenta de esta restricción temporal a la que se ven sujetos los vecinos
+# y vecinas de la Ciudad.
 
-# Abrimos nuestro dataset con los espacios verdes (EV) de la Ciudad de Buenos Aires.
+# Abrimos nuestro dataset con los espacios verdes de la Ciudad de Buenos Aires.
 verdes <- st_read("data/processed/GCABA/EV/espacios-verdes-CABA-cualificados.shp")
 
 #A partir del dataset, anterior creamos dos datasets: 
 # - Espacios verdes abiertos.
 abiertos <- verdes %>% 
-    filter(cierre!="Cerrable")
+    filter(cierre != "Cerrable")
 
 # - Espacios verdes cerrados (EVC).
 cerrados <- verdes %>% 
-    filter(cierre=="Cerrable")
+    filter(cierre == "Cerrable")
 
 
-# ¿Cómo subponderamos la superficie de los espacios verdes cerrables (EVC)?
+## ¿Cómo subponderamos la superficie de los espacios verdes cerrables (EVC)?
 
 # 1º Extraemos las geometrías de nuestro dataset.
 cerrados_sfc <- st_geometry(cerrados)
 
-# 2º Para subponderar nuestras geometrías necesitamos un punto desde donde escalarlas.
+# 2º Para subponderar nuestras geometrías, necesitamos un punto desde donde escalarlas.
 # Para ello, obtenemos los centroides.
 cerrados_sfc_centroides <- st_centroid(cerrados_sfc)
 
@@ -45,12 +46,12 @@ sfc_desde_centroide <- (cerrados_sfc - cerrados_sfc_centroides)
 # se encuentran abiertas durante el día. En este marco, si se sostiene que la mayor 
 # intensidad de uso de un espacio público ocurre entre 6 hs y 24 hs, se puede pensar
 # que un parque cercado resulta inaccesible durante un tercio de su uso potencial.
-# Por este motivo, de este razonamiento, se desprende que la falta de accesibilidad
-# a los mismos puede ser penalizada a un 33% la superficie del espacio cerrable.
+# Por este motivo, de este razonamiento se desprende que la falta de accesibilidad
+# a los mismos puede ser penalizada a un 33%  de la superficie total del espacio cerrable.
 ponderador <- 0.67
 
 # 5º multiplicamos nuestra superficie por el ponderador y le sumamos las coordenadas del centroide.
-cerrados_scala <- sfc_desde_centroide*ponderador + cerrados_sfc_centroides
+cerrados_scala <- sfc_desde_centroide * ponderador + cerrados_sfc_centroides
 
 # 6º Remplazamos nuestras geometrías viejas por nuestra nueva superficie ponderada.
 cerrados <- cerrados %>% 
@@ -68,10 +69,11 @@ leaflet(cerrados) %>%
 # Volvemos a juntar nuestros datasets de espacios abiertos y cerrados en uno que contenga ambos.
 verdes_cualificados <- rbind(abiertos, cerrados)
 
-# Visualización
+# Visualización.
 leaflet(verdes_cualificados) %>% 
     addPolygons() %>% 
     addTiles()
+
 
 # Guardamos
 st_write(verdes_cualificados, "data/processed/GCABA/EV/espacios-verdes-ponderados.shp", delete_dsn = TRUE)
